f91c922c1229494080fac4e26d05827fab618ea2
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-06-21 01:46:28 +0800

    test

--- src/Fallback.tsx
+++ src/Fallback.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 export default function Fallback() {
     return (
-        <main className="text-center font-thin mx-auto container flex flex-col p-6 text-wrap bg-black">
+        <main className="text-center font-thin mx-auto container flex flex-auto p-6 text-wrap bg-black">
             <h1><strong className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500">Please wait while we are loading...</strong></h1>
         </main>
     );
